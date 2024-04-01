5422467135be892e81467aba78eb56f1398bc0a5
amirrezaask <raskarpour@gmail.com>
2024-04-01 19:40:23 +0330

    edit command

--- go.mod
+++ go.mod
@@ -3,7 +3,8 @@ module github.com/amirrezaask/totpgen
 go 1.22.0
 
 require (
-	github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
-	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f // indirect
-	github.com/pquerna/otp v1.4.0 // indirect
+	github.com/kirsle/configdir v0.0.0-20170128060238-e45d2f54772f
+	github.com/pquerna/otp v1.4.0
 )
+
+require github.com/boombuler/barcode v1.0.1-0.20190219062509-6c824513bacc // indirect
--- main.go
+++ main.go
@@ -3,9 +3,13 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"flag"
 	"fmt"
 	"os"
+	"os/exec"
 	"path/filepath"
+	"runtime"
+	"strings"
 	"time"
 
 	"github.com/kirsle/configdir"
@@ -59,21 +63,83 @@ func readConfig(path string) (Config, error) {
 	return cfg, nil
 }
 
-// TODO:
-// Edit command to edit configuration in system editor
-func main() {
+func generateCmd() error {
 	path := configdir.LocalConfig()
 	configPath := filepath.Join(path, "totpgen.json")
 	cfg, err := readConfig(configPath)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	t := time.Now()
 	for _, acc := range cfg {
 		code, err := totp.GenerateCode(acc.Secret, t)
 		if err != nil {
-			panic(err)
+			return err
 		}
-		fmt.Printf("%s :: %s\n", acc.Name, code)
+		fmt.Printf("%s => %s\n", acc.Name, code)
+	}
+
+	return nil
+}
+
+func getEditorExecCommand() *exec.Cmd {
+	EDITOR := os.Getenv("EDITOR")
+	VISUAL := os.Getenv("VISUAL")
+	if EDITOR != "" {
+		segs := strings.Split(EDITOR, " ")
+		editorProgram := segs[0]
+		var args []string
+		if len(segs) > 1 {
+			args = segs[1:]
+		}
+		return exec.Command(editorProgram, args...)
+	} else if VISUAL != "" {
+		segs := strings.Split(VISUAL, " ")
+		editorProgram := segs[0]
+		var args []string
+		if len(segs) > 1 {
+			args = segs[1:]
+		}
+		return exec.Command(editorProgram, args...)
+	} else if runtime.GOOS == "windows" {
+		_, err := exec.LookPath("code")
+		if err != nil {
+			return exec.Command("notepad")
+		}
+		return exec.Command("code", "-w")
+	} else {
+		return exec.Command("nano")
+	}
+}
+
+func editCmd() error {
+	path := configdir.LocalConfig()
+	configPath := filepath.Join(path, "totpgen.json")
+	editor := getEditorExecCommand()
+	editor.Args = append(editor.Args, configPath)
+
+	if err := editor.Run(); err != nil {
+		return err
+	}
+	return nil
+}
+
+// TODO:
+// Edit command to edit configuration in system editor
+func main() {
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1 {
+		if err := generateCmd(); err != nil {
+			panic(fmt.Sprintf("running generate cmd failed: %s", err.Error()))
+		}
+	} else {
+		cmd := args[0]
+		switch cmd {
+		case "edit":
+			if err := editCmd(); err != nil {
+				panic(fmt.Sprintf("running edit cmd failed: %s", err.Error()))
+			}
+		}
 	}
 }
